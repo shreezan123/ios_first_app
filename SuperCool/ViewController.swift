1ce2e2112a8d995192b6d20224647492dbba5f2b
Shrijan Aryal <shrijan.aryal123@gmail.com>
2016-06-18 18:33:46 -0400

    This is the entire application

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackGround: UIImageView!
+   
+    @IBOutlet weak var AppLogo: UIImageView!
+    @IBOutlet weak var Makemecool: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Coollooks(sender: AnyObject) {
+        
+        BackGround.hidden = false
+        AppLogo.hidden = false
+        Makemecool.hidden = true
+        }
 }
 
